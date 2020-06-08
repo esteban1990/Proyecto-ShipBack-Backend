30a784671ce819eb537be7da1e9d3c7a355bf6f7
Emilio_Ballesta <efballes@uc.cl>
2020-06-08 19:44:29 -0400

    models and changes in settings.json and pipfile to run the model

--- src/app.py
+++ src/app.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from models import db
 
 app = Flask(__name__)
+#app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:123.admin@localhost/ejemplo"
 
 if __name__ == '__main__':
   app.run(host='127.0.0.1', port=3245, debug=True)
---
+++ src/models.py
@@ -0,0 +1,98 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+#Me traje la clase person y user del ejemplo visto en el curso por si se utiliza para generar el log in.
+
+class Person(db.Model):
+   id = db.Column(db.Integer, primary_key=True)
+   name = db.Column(db.String(100), nullable=False)
+   last_name = db.Column(db.String(100), nullable=False)
+   active = db.Column(db.Boolean, nullable=False)
+   users = db.relationship('User', backref='person', lazy=True)
+
+   def serialize(self):
+     return {
+       "id": self.id,
+       "name": self.name,
+       "last_name": self.last_name,
+       "active": self.active,
+     }
+   def getFullName(self):
+     return {
+       "id": self.id,
+       "full_name": self.name + " " + self.last_name
+     }
+
+class User(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  email = db.Column(db.String(255), nullable=False)
+  password = db.Column(db.String(255), nullable=False)
+  person_id = db.Column(db.Integer, db.ForeignKey('person.id'), nullable=True)
+  order = db.relationship('Order', backref='user', lazy=True)
+
+  def serialize(self):
+    return {
+      "id": self.id,
+      "email": self.email,
+      "password": self.password
+    }
+
+class Petition(db.Model):
+     id = db.Column(db.Integer, primary_key=True)
+     email = db.Column(db.String(10), nullable=False)
+     phone_number = db.Column(db.Integer(10), nullable=False)
+     description = db.Column(db.String(50), nullable=True)
+     change_or_return = db.Column(db.Boolean, nullable=False) #Si es falso es porque es devolución.
+     bought_product = db.relationship('boughtProduct', backref='Petition', lazy=True)
+     change_product = db.relationship('Change', backref='Petition', lazy=True)
+     return_product = db.relationship('Return', backref='Return', lazy=True)
+
+class boughtProduct(db.Model):
+     id = db.Column(db.Integer, primary_key=True)
+     name = db.Column(db.String(50), nullable=False)
+     price = db.Column(db.Integer, nullable=False)
+     selected = db.Column(db.Boolean, nullable=False)
+     description = db.Column(String(50), nullable=True)
+     petition_id = db.Column(db.Integer, db.ForeignKey('Petition.id'), nullable=True)
+
+class Change(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    change_product = db.Column(db.String(50), nullable=True)
+    state = db.Column(db.String(50), nullable=True)
+    city = db.Column(db.String(50), nullable=True)
+    address = db.Column(db.String(50), nullable=True)
+    commune = db.Column(db.String(50), nullable=True)
+    petition_id = db.Column(db.Integer, db.ForeignKey('Petition.id'), nullable=True)
+
+class Return(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    bank = db.Column(db.String(50), nullable=True)
+    account_type = db.Column(db.String(50), nullable=True)
+    account_number = db_Column(db.Integer, nullable=True)
+    petition_id = db.Column(db.Integer, db.ForeignKey('Petition.id'), nullable=True)
+
+class Order(db.Model):
+     id = db.Column(db.Integer, primary_key=True)
+     entrepreneur_name = db.Column(db.String(10), nullable=False)
+     entrepreneur_lastname = db.Column(db.String(10), nullable=False)
+     entrepreneur_email = db.Column (db.String(10), nullable=False)
+     client_name = db.Column(db.String(10), nullable=False) 
+     client_lastname = db.Column(db.String(10), nullable=False)
+     client_email = db.Column(db.String(10), nullable=False) 
+     booked_date = db.Column(db.DateTime, nullable=False)
+     city = db.Column(db.String(10), nullable=False)
+     state = db.Column(db.String(10), nullable=False)
+     courrier = db.Column(db.String(10), nullable=False)
+     cost = db.Column(db.Integer, nullable=False)
+     number_of_packages = db.Column(db.Integer, nullable=False)
+     invoice_number = db.Column(db.Integer, nullable=False)
+     postCode = db.Column(db.Integer, nullable=False)
+     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=True)
+
+class Billing_details(db.Model):
+     id = db.Column(db.Integer, primary_key=True)
+     cardNumber = db.Column(db.Integer, nullable=False)
+     cvv = db.Column(db.Integer, nullable=False)
+     expiration_date = db.Column(db.String(10), nullable=False)
+     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=True)
