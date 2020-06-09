a6bd25ff867b73a8f56f500ea90456e6d06419ad
esteban1990 <55974184+esteban1990@users.noreply.github.com>
2020-06-08 20:06:55 -0400

    fase-1 autenticacion signUp-Login

---
+++ src/models.py
@@ -0,0 +1,37 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Person(db.Model):
+    __tablename__ = 'person'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(10), nullable=False)
+    lastname = db.Column(db.String(10), nullable=False)
+    email = db.Column(db.String(10), nullable=False)
+    password = db.Column(db.String(6), nullable=False)
+    users = db.relationship("User", backref="person", lazy=True)
+    
+
+    def serialize(self):
+        return{
+            "id": self.id,
+            "name": self.name,
+            "lastname": self.lastname,
+            "email": self.email,
+            "password": self.password
+        }
+
+
+class User(db.Model):
+    id = db.Column(db.Interger, primary_key=True)
+    email = db.Column(db.String(255), nullable=False)
+    password= db.Column(db.String(255), nullable=False)
+    person_id = db.Column(db.Interger, db.ForeignKey("person.id"), nullable=True)
+
+
+    def serialize(self):
+        return{
+            "id":self.id,
+            "email":self.email,
+            "password":self.password
+        }
